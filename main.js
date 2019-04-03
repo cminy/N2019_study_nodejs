d781801cc6b53f51eae255fdbbc620d3f44af21a
mignon.choi <mksuuup@gmail.com>
2019-04-03 16:22:22 +0900

    파일로본문받기

--- main.js
+++ main.js
@@ -13,32 +13,31 @@ var app = http.createServer(function(request,response){
     }
     if(urlm == '/favicon.ico'){
       response.writeHead(404);
-      // response.end();
-      // return;
     }
     response.writeHead(200);
 
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=html">HTML</a></li>
-        <li><a href="/?id=css">CSS</a></li>
-        <li><a href="/?id=javascript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p style="margin-top:45px;"">The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989.
-      </p>
-    </body>
-    </html>
-    `;
-
+    fs.readFile(`data${queryData.id}`, 'utf-8', function(err, description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=html">HTML</a></li>
+          <li><a href="/?id=css">CSS</a></li>
+          <li><a href="/?id=javascript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p style="margin-top:45px;"">
+          ${description}
+        </p>
+      </body>
+      </html>
+      `;
     response.end(template);
 });
 app.listen(3000); //3000은 포트넘버.
