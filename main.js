3fb8bbd77f7f97896e3de1cbc28d3bc18102dcdc
mignon.choi <mksuuup@gmail.com>
2019-04-09 11:34:17 +0900

    undefind 뭐가문제일까

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ var app = http.createServer(function(request,response){
     }
 */
     if(pathname === '/'){
-      fs.readFile(`data/${title}`, 'utf-8', function(err, description){
+      fs.readFile(`data/${title}`, 'utf8', function(err, description){
         var template = `
         <!doctype html>
         <html>
