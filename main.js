76f620bbd47dfcf6b034b62edb61f91bba4df8dc
mignon.choi <mksuuup@gmail.com>
2019-04-10 12:10:44 +0900

    수정해따

--- main.js
+++ main.js
@@ -3,18 +3,6 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateList(filelist){
-  var i = 0;
-  var files = '</ol>';
-  while (i < filelist.length){
-    files = files + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
-  }
-  files = files + '</ol>';
-//  console.log(files);
-  return files;
-}
-
 function templateHtml(title, list, body){
   return`
     <!doctype html>
@@ -33,32 +21,43 @@ function templateHtml(title, list, body){
     `;
 }
 
+function templateList(filelist){
+  var i = 0;
+  var files = '</ol>';
+  while (i < filelist.length){
+    files = files + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  files = files + '</ol>';
+//  console.log(files);
+  return files;
+}
+
 var app = http.createServer(function(request,response){
     var urlm = request.url;
     var queryData = url.parse(urlm,true).query;
     var pathname = url.parse(urlm, true).pathname;
     var title = queryData.id;
 
-
     fs.readdir('data', function(err,filelist){
+      var files = templateList(filelist);
+
       fs.readFile(`data/${title}`,'utf8',function(err,description){
-        var list = templateList(filelist);
-        var template=templateHtml(title, list, body)
+        var template=templateHtml(title, files, description)
 
         if(pathname === '/'){ //home으로 갔을 때
           if(title === undefined){ //welcome 페이지 설정
             title = 'Welcome';
             description = 'Hello, Node.js';
           }
-          template =templateHtml(title,list,`<h2>${title}</h2><p style="margin-top:45px;"">${description}</p>`);
+          template =templateHtml(title,files,`<h2>${title}</h2><p style="margin-top:45px;"">${description}</p>`);
 
           response.writeHead(200);
           response.end(template);
         }
         else if(pathname === '/create'){
           title = 'WEB - Create';
-          list = templateList(filelist);
-          template =templateHtml(title,list,`
+          template =templateHtml(title,files,`
             <form action="http://localhost:3000/create_process" method="post">
               <p> <input type="text" name="title" placeholder="title" /></p>
               <p>
@@ -73,18 +72,20 @@ var app = http.createServer(function(request,response){
           response.end(template);
         } else if(pathname === '/create_process'){
           var body = '';
-          request.on('data',function(data){
+          request.on('data',function(data){ //data 수신할 때마다 function 실행
             body = body + data;
           });
-          request.on('end',function(){
-            var post = qs.parse(body);
+          request.on('end',function(){ // 더이상 받을 data가 없는 경우
+            var post = qs.parse(body); //qs : querystring모듈
+            console.log(post);
             var title = post.title;
             var description = post.description;
-            fs.writeFile(`data/${title}`,'utf8',function(err){
-              response.writeHead(302, {Location: `?/id=${title}`});
+            //파일생성
+            fs.writeFile(`data/${title}`,description,'utf8',function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
               response.end();
             })
-          })
+          });
         } else {
         response.writeHead(404);
         response.end('Not Found');
